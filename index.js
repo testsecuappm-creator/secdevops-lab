58123a67b8180bf4ac2acd890c51e638c8d16ce4
maria abrini <mabrini@freylab.local>
2026-07-27 11:56:44 +0200

    Revert "Test: add realistic fake secret to verify detection"
    This reverts commit 0530978ea43499c5570dc9825a83ce9190537b38.

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => console.log('Listening on port 3000'));
 
 // Test - fake secret for lab purposes
-const AWS_ACCESS_KEY_ID = "AKIAT7XJ4KP9M2QRSTUV";
+const AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE";
